eb82f1bfc0d553b27146dcf26a6f203f0fa994ac
campNav <phillipr.yorku@gmail.com>
2017-04-18 14:45:07 -0400

    Add files via upload

---
+++ MAIN.m
@@ -0,0 +1,10 @@
+% Main run
+
+cd('C:\Users\Phill-GeoICT\Desktop\System\1. Search Hash Table')
+run('BSGeometricHashingSearch.m')
+
+cd('C:\Users\Phill-GeoICT\Desktop\System\2. Point Correspondance Between Images')
+run('PointCorrespondance.m')
+
+cd('C:\Users\Phill-GeoICT\Desktop\System\3. Calculate position')
+run('TabulatePosition.m')
